c0150d35c09dc940e565837125c00e7cac5c6b9b
hindenbug <manoj.mk27@gmail.com>
2020-04-12 12:37:28 +0200

    Implement AST

---
+++ src/ast.rs
@@ -0,0 +1,60 @@
+use crate::token::Token;
+pub trait Node {
+    fn token_literal(&self) -> &str;
+}
+
+pub trait Statement: Node {
+    fn statement_node(&self);
+}
+
+pub trait Expression: Node {
+    fn expression_node(&self);
+}
+
+pub struct Program {
+    pub statements: Vec<Box<dyn Statement>>,
+}
+
+impl Node for Program {
+    fn token_literal(&self) -> &str {
+        match self.statements.len() {
+            0 => "",
+            _ => self.statements[0].token_literal(),
+        }
+    }
+}
+
+pub struct LetStatement {
+    token: Token,
+    name: Identifier,
+    value: Expression,
+}
+
+impl Statement for LetStatement {
+    fn statement_node(&self) {
+        unimplemented!();
+    }
+}
+
+impl Node for LetStatement {
+    fn token_literal(&self) -> &str {
+        self.token.literal.as_str()
+    }
+}
+
+pub struct Identifier {
+    token: Token,
+    value: String,
+}
+
+impl Expression for Identifier {
+    fn expression_node(&self) {
+        unimplemented!();
+    }
+}
+
+impl Node for Identifier {
+    fn token_literal(&self) -> &str {
+        self.token.literal.as_str()
+    }
+}
